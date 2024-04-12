0f6fb1ef193ba63194ebf87868eb485bcd0070fa
jKilit <Jonathan.kilit@hotmail.com>
2024-04-12 08:48:51 +0200

    Initial Commit

---
+++ Chatgpt-app/Chatgpt_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chatgpt_appApp.swift
+//  Chatgpt-app
+//
+//  Created by Jonathan Kilit on 2024-04-12.
+//
+
+import SwiftUI
+
+@main
+struct Chatgpt_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chatgpt-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chatgpt-app
+//
+//  Created by Jonathan Kilit on 2024-04-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
